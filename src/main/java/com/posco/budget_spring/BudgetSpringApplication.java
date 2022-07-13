17b83e11d656a54c2891b17755e1fbecf06a5001
HjuH <hjuhee9807@naver.com>
2022-07-13 17:36:50 +0900

    Initial commit

---
+++ src/main/java/com/posco/budget_spring/BudgetSpringApplication.java
@@ -0,0 +1,13 @@
+package com.posco.budget_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/posco/budget_spring/BudgetSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posco.budget_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
